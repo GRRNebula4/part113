a26540eee7b600287f59d06d9d7a8dc3fe99cd12
Katainen Pähkinä <s2400324@BC-C02FV0BVQ6LTs-MacBook-Air.local>
2024-11-25 02:31:50 +0200

    base: folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/GRRNebula4/part113",
+  base: "/part113",
   plugins: [react()],
 })

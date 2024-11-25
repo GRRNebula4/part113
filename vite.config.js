0a78e9a9074d6710dc6714a3707a149a8bc16cef
Katainen Pähkinä <s2400324@BC-C02FV0BVQ6LTs-MacBook-Air.local>
2024-11-25 02:28:09 +0200

    yritys 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://github.com/GRRNebula4/part113
+// https://github.com/GRRNebula4/part113/
 export default defineConfig({
   plugins: [react()],
 })
